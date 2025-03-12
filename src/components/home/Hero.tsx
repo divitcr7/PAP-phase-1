8fabdcb50cc2a2688b7797fed518a320db77bdac
ubednama <shahidkhan.ak18@gmail.com>
2025-03-12 20:13:41 +0530

    refactor Hero component for improved styling and layout consistency

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,6 +4,7 @@ import { Home, Building2, Landmark, Factory, ArrowRight } from "lucide-react";
 import { Link } from "react-router";
 import BGSliderImage from "/images/slider/slider-1.jpg";
 import { Typewriter } from "react-simple-typewriter";
+import { cn } from "@/lib/utils";
 
 export default function Hero() {
   return (
@@ -33,39 +34,52 @@ export default function Hero() {
             </p>
             <Button
               asChild
-              className="mt-6 rounded-full px-8 py-4 md:px-10 md:py-7 text-base sm:text-lg font-semibold bg-blue-600 hover:bg-primary-dark"
+              className={cn(
+                "mt-6 rounded-full text-base sm:text-lg font-semibold bg-blue-600 hover:bg-blue-700 !p-6",
+              )}
             >
-              <Link to="/contact">Contact Us
-              {/* <ArrowRight /> */}
+              <Link to="/contact" className="">
+                Contact Us
+                <ArrowRight className="ml-2" />
               </Link>
             </Button>
           </div>
           <div className="w-full lg:w-auto">
             <PropertyFilter />
             <div className="mt-6 flex flex-wrap justify-center gap-4">
-              <Link to="#" className="flex items-center gap-1 md:gap-2 text-white">
+              <Link
+                to="#"
+                className="flex items-center gap-1 md:gap-2 text-white"
+              >
                 <div className="bg-white/20 hover:bg-white/30 rounded-full p-1">
                   <Home className="text-white size-4 md:size-6" />
                 </div>
                 Houses
               </Link>
 
-              <Link to="#" className="flex items-center gap-1 md:gap-2 text-white">
+              <Link
+                to="#"
+                className="flex items-center gap-1 md:gap-2 text-white"
+              >
                 <div className="bg-white/20 hover:bg-white/30 rounded-full p-1 md:p-2 text-sm md:text-base">
-                  <Building2
-                    className="text-white size-4 md:size-6"
-                  />
+                  <Building2 className="text-white size-4 md:size-6" />
                 </div>
                 Villa
               </Link>
 
-              <Link to="#" className="flex items-center gap-1 md:gap-2 text-white">
+              <Link
+                to="#"
+                className="flex items-center gap-1 md:gap-2 text-white"
+              >
                 <div className="bg-white/20 hover:bg-white/30 rounded-full p-1 md:p-2 text-sm md:text-base">
                   <Landmark className="text-white size-4 md:size-6" />
                 </div>
                 Office
               </Link>
-              <Link to="#" className="flex items-center gap-1 md:gap-2 text-white">
+              <Link
+                to="#"
+                className="flex items-center gap-1 md:gap-2 text-white"
+              >
                 <div className="bg-white/20 hover:bg-white/30 rounded-full p-1 md:p-2 text-sm md:text-base">
                   <Factory className="text-white size-4 md:size-6" />
                 </div>
